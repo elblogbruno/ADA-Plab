5a7ffff2d43b77a1389b27f7a7420acb45020dc4
marcgarrofe <mgarrofeurrutia@gmail.com>
2021-11-19 10:29:18 +0100

    Update BranchAndBound.cpp

--- GraphApplication/BranchAndBound.cpp
+++ GraphApplication/BranchAndBound.cpp
@@ -4,12 +4,117 @@
 #include <iostream>
 #include <iomanip> 
 
+#include <numeric>
 
+void initDistanceAndTrackMatrix(CGraph &graph, CVisits& visits, vector<vector<double>>& distance_matrix, vector<vector<CTrack*>>& track_matrix) {
+	// Fem resize amb NxN on N=nº de nodes a visitar
+	distance_matrix.resize(visits.GetNVertices(), vector<double>(visits.GetNVertices()));
+	track_matrix.resize(visits.GetNVertices(), vector<CTrack*>(visits.GetNVertices()));
+
+	// Inicialitzacio matrius distance_matrix i track_matrix amb els valors corresponents
+	int row = 0;
+	int col = 0;
+	for (CVertex* vertex : visits.m_Vertices) {
+		col = 0;
+		// Cridem Dijkstra per a cada node del Graph
+		DijkstraQueue(graph, vertex);
+		for (CVertex* vertex_2 : visits.m_Vertices) {
+			CTrack* aux = new CTrack(&graph);
+			CVertex* actV = vertex_2;
+			while (actV != vertex) {
+				aux->AddFirst(actV->m_DijkstraAnterior);
+				actV = actV->m_DijkstraAnterior->m_pOrigin;
+			}
+			if (vertex != vertex_2) {
+				track_matrix[col][row] = aux;
+				distance_matrix[col][row] = aux->Length();
+			}
+			col++;
+		}
+		row++;
+	}
+}
+
+// CBBNode ===============================================================
+
+class CBBNode {
+public:
+	double m_Length;
+	double m_WeigthMin; // Cota inferior. peso del nodo a más pesado más largo es su posible camino
+	int m_VisitesFetes;
+	vector<int> m_VectorIndexVisites;	// Guardem index de les visites per accedir a la matriu
+	int m_indexNode;
+
+	/*
+	Exemple :
+	t = 0
+		int m_VisitesFetes = 3
+		vector<int> m_IndexVisites -> 0 | 2 | 5 | 1 | 3 | 4 | 6
+			Deduim que les visites fetes són : 0 | 2 | 5
+			I les restants són : 1 | 3 | 4 | 6
+
+	t = 1
+		int m_VisitesFetes = 4
+		vector<int> m_IndexVisites -> 0 | 2 | 5 | 1 | 3 | 4 | 6
+			Deduim que les visites fetes són : 0 | 2 | 5 | 1
+			I les restants són : 3 | 4 | 6
+		
+	*/
+
+
+	// Pointer al pare? -> Bruno diu que es B&B V3
+
+public:
+	CBBNode(int indexNode, const int indexPare, vector<int> m_IndexVisites, double lenght, const vector<vector<double>>& distance_matrix)
+		: m_indexNode(indexNode)
+	{
+		m_IndexVisites.push_back(indexNode);
+		m_Length = lenght + distance_matrix[indexPare][indexNode];
+	}
+	CBBNode(const CBBNode& node, const vector<vector<double>> distance_matrix, int indexNode, int indexArrayDeVisites)
+		: m_indexNode(indexNode)
+		, m_VectorIndexVisites(node.m_VectorIndexVisites)
+	{
+		m_Length = node.m_Length + distance_matrix[node.m_indexNode][indexNode];
+		m_VisitesFetes = node.m_VisitesFetes + 1;
+		iter_swap(m_VectorIndexVisites.begin() + m_VisitesFetes + 1, m_VectorIndexVisites.begin() + indexArrayDeVisites);
+	}
+};
+
+// comparator ==================================================================
+
+struct comparator {
+	bool operator()(const CBBNode* s1, const CBBNode* s2) {
+		return s1->m_WeigthMin > s2->m_WeigthMin;
+	}
+};
 
 // SalesmanTrackBranchAndBound1 ===================================================
 
+/* Completar el camí més curt */
 CTrack SalesmanTrackBranchAndBound1(CGraph& graph, CVisits& visits)
 {
+	// Definim vector distàncies i camins
+	vector<vector<double>> distance_matrix;
+	vector<vector<CTrack*>> track_matrix;
+	initDistanceAndTrackMatrix(graph, visits, distance_matrix, track_matrix);
+
+	// Inicialitzem array de visitas amb seqüència del 0 al n, on n són el número de visites
+	vector<int> index_visits;
+	index_visits.resize(visits.GetNVertices());
+	iota(begin(index_visits), end(index_visits), 0);
+
+	// Definim node inicial o arrel
+	CBBNode node_inicial(0, 0, index_visits, 0, distance_matrix);
+	// Definim punter per recorrer i expandir l'arbre
+	CBBNode node_actual = node_inicial;
+
+	for (int node_seguent = node_actual.m_VisitesFetes; node_seguent < node_actual.m_VectorIndexVisites.size(); node_seguent++) {
+
+	}
+
+
+
 	return CTrack(&graph);
 }
 
